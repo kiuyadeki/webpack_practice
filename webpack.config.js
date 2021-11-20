af4736e685f1b80cdd6239e82c4d371310fbc874
kiuyadeki <60567481+kiuyadeki@users.noreply.github.com>
2021-11-20 16:17:01 +0900

    Install css-loader & style-loader

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path'); //node.jsにデフォルトで入っているpathモジュールを使用
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'), //path.resolveで絶対パスを取得、第一引数(__dirname)が現在の階層を表す
+        filename: 'main.js', // 出力されるファイル名
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/, //.cssを検知する
+                use: [
+                    {
+                        loader: 'style-loader', //loaderは下から上に適用されるので、css-loader, style-loaderの順になる。
+                    },
+                    {
+                        loader: 'css-loader' //.cssが見つかれば、このルールを適用する。
+                    },
+                ],
+            },
+        ],
+    },
+}
